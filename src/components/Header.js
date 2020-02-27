5ff01279f8c89ce01b9d8b849c97763e9660bb83
Jorge Morana <JMorana@slb.com>
2020-02-27 15:53:34 -0600

    Initial

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Header = () => {
+  return (
+    <header className="bg-alert">
+      <h1>Recetas de Bebidas</h1>
+    </header>
+  );
+};
+
+export default Header;
